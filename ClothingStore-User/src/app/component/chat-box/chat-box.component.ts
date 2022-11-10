0719d5cddbc406bb1363a4371f45f94491921003
ngoclam2000 <lam.bui@dotnets.org>
2022-11-11 05:53:52 +0700

    chat

--- ClothingStore-User/src/app/component/chat-box/chat-box.component.ts
+++ ClothingStore-User/src/app/component/chat-box/chat-box.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { ChatService } from 'src/app/services/chat.service';
+ 
+import Pusher from 'pusher-js';
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
@@ -7,12 +10,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatBoxComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private chatService:ChatService) { }
+  listMessage:any
+ currentMessage:any
+ currentAvatarUser:any
+ message:string=""
+ currentMessageId:any
+ isSeen:boolean=true
   ngOnInit(): void {
+
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('05ba42f251be5a21e7fa', {
+      cluster: 'ap1'
+    });
+
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', data => {
+  
+      
+ 
+      if(data.chatId==this.currentMessageId)
+      this.listMessage.push(data.message);
+  
+    });
+   
     var messageBody = <HTMLVideoElement>document.querySelector('#parentDiv');
-    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight+100;
+ 
+ 
+    
+    
+ 
+
+
      
+    
+    this.getMessage()
   }
+  getMessage()
+  {
+    this.chatService.getMessage(localStorage.getItem("userId")).subscribe(res=>
+      {
+ 
+        this.currentMessage=res
+    
+        this.currentMessageId=this.currentMessage[0].chatId;
+    
+        this.currentMessage=this.currentMessage[0].message
+      this.listMessage=this.currentMessage.split("|")
+   
+
+        
+
+
+      })
+  }
+  checkMes(mes:any)
+  {  
+   
+    if(mes.split(",")[0]=="admin")
+    { 
+       
+  
+      mes.split(",")[0]=="admin"
+      return true;
+    } 
+    else 
+    {
+     
+      return false;
+    }
+  }
+  formatDate(date:any)
+  {
+    if(new Date().getDate()==new Date(date).getDate() && new Date().getMonth()==new Date(date).getMonth() && new Date().getFullYear== new Date(date).getFullYear )
+    return "Hôm nay"
+    if(new Date().getDate()==new Date(date).getDate()+1 && new Date().getMonth()==new Date(date).getMonth() && new Date().getFullYear== new Date(date).getFullYear )
+    return "Hôm qua"
+    var d=new Date(date).getDate() + "/"+ (new Date(date).getMonth()+1).toString() +"/" +new Date(date).getFullYear()
+    
+    return d
+  }
+  sendMessage()
+  {
+   var data={
+      id: this.currentMessageId,
+      message: this.message,
+      isAdmin:false
+    }
+ 
+     this.chatService.sendMessage(data).subscribe(res=>{
+       this.message=""
+   
+    })
+  }
+
 
 }
---
+++ ClothingStore-User/src/app/services/chat.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  apiUrl=environment.apiUrl+"/ChatUser"
+
+  constructor(private http: HttpClient) { }
+ 
+  getMessage(id:any)
+  {
+    return this.http.get(this.apiUrl+"/get-message-by-userid/"+id)
+  }
+  sendMessage(message:any)
+  {
+ 
+    return this.http.put(this.apiUrl+"/send-message",message)
+  }
+}
